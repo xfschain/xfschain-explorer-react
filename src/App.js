121bb0ad8da66685025eb49521b321c26060b503
liuyingjie <liuyingjie@dsyun.io>
2022-06-27 23:19:40 +0800

    update

--- src/App.js
+++ src/App.js
@@ -374,7 +374,7 @@ class App extends React.Component {
         <div className="container">
           <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
             <p className="col-md-4 mb-0 text-muted">Copyright &copy; {new Date().getFullYear()} Starx Labs, Inc</p>
-            <ul className="nav col-md-4 justify-content-end">
+            <ul className="nav col-md-6 justify-content-end">
               <li className="nav-item">
                 <a href="/" className="nav-link px-2 text-muted">
                   {intl.get('NAV_HOME')}
